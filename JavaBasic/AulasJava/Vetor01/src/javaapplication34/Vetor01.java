0232ab4daf19131137d31e321679ae2f0f895baf
Nykoo <99055263+NykooLicar@users.noreply.github.com>
2022-09-17 16:45:14 -0300

    Changes

---
+++ JavaBasic/AulasJava/Vetor01/src/javaapplication34/Vetor01.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication34;
+
+/**
+ *
+ * @author paulo
+ */
+public class Vetor01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n[] = {3,2,8,7,5,4};
+        
+        for (int c=0; c<=n.length-1; c++) {
+        System.out.println("Na posição " + c + " Temos o valor " + n[c]);
+            
+        }
+    }
+    
+}
---
+++ JavaBasic/ExerciciosJava/Repeticao01/src/repeticao01/Repeticao01.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repeticao01;
+
+/**
+ *
+ * @author paulo
+ */
+public class Repeticao01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int cc = 1;
+        do {
+            System.out.println("Repeticao " + cc);
+            cc++;
+        } while (cc <= 4);
+        
+    }
+    
+}
